09fa76aa5c666e513c8d708636d62d55f70ff745
OniDem1 <OniDem1@ONIDEM1-NOTEBOO>
2023-02-17 16:09:41 +0700

    Добавьте файлы проекта.

---
+++ Lesson/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Lesson
+{
+    class Program
+    {
+        interface IVehiculo
+        {
+
+            bool Refuel(int fuel);
+            void Drive();
+        }
+
+        class Car : IVehiculo
+        {
+            private int  _fuel;
+
+            public int Fuel
+            {
+                get
+                {
+                    return _fuel; 
+                }
+                set
+                {
+                    _fuel = value;
+                }
+            }
+
+            public Car(int fuel) 
+            {
+                Fuel = fuel;
+            }
+
+            public void Drive()
+            {
+                if (Fuel < 0)
+                {
+                    Console.WriteLine("You can't drive");
+                }
+                else
+                {
+                    Console.WriteLine("Car driving");
+                    Fuel--;
+                }
+            }
+
+            public bool Refuel(int fuel)
+            {
+                if (Fuel > 0)
+                {
+                    Fuel = fuel;
+                    return true;
+                }
+                else
+                    Fuel = fuel;
+                return false;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Car car = new Car(0);
+            while (true)
+            {
+                if (car.Fuel == 0)
+                {
+                    Console.WriteLine("You want to refuel your car? y/n");
+                    string? choice = Console.ReadLine();
+                    if (choice == "y")
+                        car.Refuel(10);
+                    else
+                    {
+                        Console.WriteLine("You can't drive");
+                        break;
+                    }
+                }
+
+                    car.Drive();
+            }
+        }
+    }
+}
